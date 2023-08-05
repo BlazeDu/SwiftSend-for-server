e3d73d5e1e53c716f4fd4e0c6b2f478c76286004
Dump <120537913+TechnologyWGJ@users.noreply.github.com>
2023-08-05 20:21:31 +0800

    Fix: 🐛修复安全漏洞

--- web/Upload.js
+++ web/Upload.js
@@ -5,30 +5,55 @@ function Upload() {
         const selectedFile = event.target.files[0];
         if (selectedFile) {
             // 获取最大文件大小配置
-            fetch('/api/getMaxFileSize')
+            fetch('/api/CheckFileSize')
                 .then(response => response.json())
-                .then(config => {
-                    if (config.File.CheckFileSize)
-                        if (selectedFile.size > config.File.MaxFileSize * 1024 * 1024 * 1024) {
-                            alert(`文件过大，已超过${config.File.MaxFileSize}GB`);
-                            return;
-                        }
-                    const formData = new FormData();
-                    formData.append('file', selectedFile);
-                    fetch('/upload', {
-                        method: 'POST',
-                        body: formData,
-                    })
-                        .then((response) => response.json())
-                        .then((data) => {
-                            alert('上传成功，验证码为: ' + data.verificationCode);
+                .then(data => {
+                    const CheckFileSize = data.CheckFileSize;
+                    if (CheckFileSize) {
+                        fetch('/api/MaxFileSize')
+                            .then(response => response.json())
+                            .then(data => {
+                                const MaxFileSize = data.MaxFileSize;
+                                if (selectedFile.size > MaxFileSize * 1024 * 1024 * 1024) {
+                                    alert(`文件过大，已超过${MaxFileSize}GB`);
+                                    return;
+                                }
+                                const formData = new FormData();
+                                formData.append('file', selectedFile);
+                                fetch('/upload', {
+                                    method: 'POST',
+                                    body: formData,
+                                })
+                                    .then((response) => response.json())
+                                    .then((data) => {
+                                        alert('上传成功，验证码为: ' + data.verificationCode);
+                                    })
+                                    .catch(() => {
+                                        alert('上传失败');
+                                    });
+                            })
+                            .catch(() => {
+                                alert("无法获取配置");
+                            });
+                    } else {
+                        // 如果后端不检查文件大小，则直接上传文件
+                        const formData = new FormData();
+                        formData.append('file', selectedFile);
+                        fetch('/upload', {
+                            method: 'POST',
+                            body: formData,
                         })
-                        .catch(() => {
-                            alert('上传失败');
-                        });
+                            .then((response) => response.json())
+                            .then((data) => {
+                                alert('上传成功，验证码为: ' + data.verificationCode);
+                            })
+                            .catch(() => {
+                                alert('上传失败');
+                            });
+                    }
                 })
                 .catch(() => {
-                    alert("请求过于频繁");
+                    alert("无法获取配置");
                 });
         }
     });
