c90ad2fa6f0d4dba709346bc3917b4bbb6def54e
Dump <stormtech.dump@gmail.com>
2023-08-16 20:30:01 +0800

    feat: 🛠️使用Vue重构上传代码

--- web/Upload.js
+++ web/Upload.js
@@ -1,61 +1,65 @@
-function Upload() {
-    const fileInput = document.createElement('input');
-    fileInput.type = 'file';
-    fileInput.addEventListener('change', (event) => {
-        const selectedFile = event.target.files[0];
-        if (selectedFile) {
-            // 获取最大文件大小配置
-            fetch('/api/CheckFileSize')
-                .then(response => response.json())
-                .then(data => {
-                    const CheckFileSize = data.CheckFileSize;
-                    if (CheckFileSize) {
-                        fetch('/api/MaxFileSize')
-                            .then(response => response.json())
-                            .then(data => {
-                                const MaxFileSize = data.MaxFileSize;
-                                if (selectedFile.size > MaxFileSize * 1024 * 1024 * 1024) {
-                                    alert(`文件过大，已超过${MaxFileSize}GB`);
-                                    return;
-                                }
-                                const formData = new FormData();
-                                formData.append('file', selectedFile);
-                                fetch('/upload', {
-                                    method: 'POST',
-                                    body: formData,
-                                })
-                                    .then((response) => response.json())
-                                    .then((data) => {
-                                        alert('上传成功，验证码为: ' + data.verificationCode);
-                                    })
-                                    .catch(() => {
-                                        alert('上传失败');
-                                    });
-                            })
-                            .catch(() => {
-                                alert("无法获取配置");
-                            });
-                    } else {
-                        // 如果后端不检查文件大小，则直接上传文件
+new Vue({
+    el: '#Send',
+    data: {
+        showAlert: false
+    },
+    methods: {
+        async Send() {
+            const fileInput = document.createElement('input');
+            fileInput.type = 'file';
+            await new Promise(resolve => {
+                fileInput.addEventListener('change', (event) => {
+                    resolve(event.target.files[0]);
+                });
+                fileInput.click();
+            });
+            if (!fileInput.files.length)
+                return;
+            try
+            {
+                const response = await fetch('/api/CheckFileSize');
+                const data = await response.json();
+                if (data.CheckFileSize) {
+                    const maxFileSizeResponse = await fetch('/api/MaxFileSize');
+                    const maxFileSizeData = await maxFileSizeResponse.json();
+                    const MaxFileSize = maxFileSizeData.MaxFileSize;
+                    if (fileInput.files[0].size > MaxFileSize * 1024 * 1024 * 1024)
+                        this.Title = `文件过大，已超过${MaxFileSize}GB`;
+                    else
+                    {
                         const formData = new FormData();
-                        formData.append('file', selectedFile);
-                        fetch('/upload', {
+                        formData.append('file', fileInput.files[0]);
+                        const uploadResponse = await fetch('/upload', {
                             method: 'POST',
                             body: formData,
-                        })
-                            .then((response) => response.json())
-                            .then((data) => {
-                                alert('上传成功，验证码为: ' + data.verificationCode);
-                            })
-                            .catch(() => {
-                                alert('上传失败');
-                            });
+                        });
+                        const uploadData = await uploadResponse.json();
+                        this.Title = "上传成功";
+                        this.Body = "验证码:" + uploadData.verificationCode;
                     }
-                })
-                .catch(() => {
-                    alert("无法获取配置");
-                });
+                }
+                else
+                {
+                    const formData = new FormData();
+                    formData.append('file', fileInput.files[0]);
+                    const uploadResponse = await fetch('/upload', {
+                        method: 'POST',
+                        body: formData,
+                    });
+                    const uploadData = await uploadResponse.json();
+                    this.Title = "上传成功";
+                    this.Body = "验证码:" + uploadData.verificationCode;
+                }
+            }
+            catch (error)
+            {
+                this.Title = "Error";
+                this.Body = "无法获取配置";
+            }
+            finally
+            {
+                this.showAlert = true;
+            }
         }
-    });
-    fileInput.click();
-}
+    }
+});
